a4f78588d438cbaa7cf97842099082e6e170cd3e
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2019-03-17 11:48:27 +0530

    SI: Injected literal values from spring bean configuration file

--- src/main/java/com/coach/app/coach/impl/GymCoach.java
+++ src/main/java/com/coach/app/coach/impl/GymCoach.java
@@ -1,21 +1,38 @@
 package com.coach.app.coach.impl;
 
-import com.coach.app.coach.Coach;
 import com.coach.app.service.FortuneService;
 
-public class GymCoach implements Coach {
+public class GymCoach {
 	private FortuneService fortuneSvc;
+	
+	private String name;
+	
+	private String eMailAddress;
 	
 	public void setFortuneSvc(FortuneService fortuneSvc) {
 		this.fortuneSvc = fortuneSvc;
 	}
+	
+	public String getName() {
+		return name;
+	}
 
-	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String geteMailAddress() {
+		return eMailAddress;
+	}
+
+	public void seteMailAddress(String eMailAddress) {
+		this.eMailAddress = eMailAddress;
+	}
+
 	public String getDailyWorkout() {
 		return "Bench press 100Kgs";
 	}
 	
-	@Override
 	public String getDailyFortune() {
 		return fortuneSvc.getFortune();
 	}
--- src/main/java/com/coach/app/driver/HelloWorldSpringApp.java
+++ src/main/java/com/coach/app/driver/HelloWorldSpringApp.java
@@ -2,7 +2,7 @@ package com.coach.app.driver;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.coach.app.coach.Coach;
+import com.coach.app.coach.impl.GymCoach;
 
 public class HelloWorldSpringApp {
 
@@ -11,11 +11,13 @@ public class HelloWorldSpringApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// retrieve bean from the spring configuration
-		Coach theCoach = context.getBean("coach", Coach.class);
+		GymCoach theCoach = context.getBean("coach", GymCoach.class);
 		
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getName());
+		System.out.println(theCoach.geteMailAddress());
 		
 		// close the context
 		context.close();
