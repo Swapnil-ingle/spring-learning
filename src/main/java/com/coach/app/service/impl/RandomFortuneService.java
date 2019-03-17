d7a92818df08b078c1ae71a0a7a03aaa6b1750ef
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2019-03-17 23:47:57 +0530

    SI: Loading the fortunes from a external text file

--- src/main/java/com/coach/app/service/impl/RandomFortuneService.java
+++ src/main/java/com/coach/app/service/impl/RandomFortuneService.java
@@ -1,21 +1,47 @@
 package com.coach.app.service.impl;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.coach.app.service.FortuneService;
 
 @Component
 public class RandomFortuneService implements FortuneService {
-	private static final String[] fortunes = {
-			"Today you're going to have a very good day", 
-			"Today, spirits are not very happy with you", 
-			"Today your day is going to be in your hands"}; 
+	@Value("${fortunes.file.path}")
+	private String fortuneFilePath;
+
+	private List<String> fortunes;
+
+	public String loadRandomFortunes() {
+		try {
+			File input = new File(fortuneFilePath);
+			BufferedReader reader = new BufferedReader(new FileReader(input));
+			String thisLine;
+			fortunes = new ArrayList<>();
+			while ((thisLine = reader.readLine()) != null) {
+				fortunes.add(thisLine);
+			}
+			reader.close();
+		} catch (Exception e) {
+			System.out.println("Error while reading from fortune file: " + e.getLocalizedMessage());
+		}
+		return getSingleRandomFortune();
+	}
 
 	@Override
 	public String getFortune() {
-		return fortunes[ThreadLocalRandom.current().nextInt(0, fortunes.length)];
+		return fortunes != null ? getSingleRandomFortune() : loadRandomFortunes();
+	}
+
+	private String getSingleRandomFortune() {
+		return fortunes.get(ThreadLocalRandom.current().nextInt(0, fortunes.size()));
 	}
 
 }
